0b37fe337b3138e5de17ff64bade20c21968c60d
zxeveroxz <s.zegarra@hotmail.com>
2022-08-06 17:11:31 -0500

    se agrego la base de datos sqlite

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const fs = require('fs');
 const path = require('path')
 const qrcode = require('qrcode-terminal');
 const qr = require('qrcode');
-const { parse } = require('path');
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db/avisos.db');
 
 const url_avisos_desague= "https://gisprd.sedapal.com.pe/arcgis/rest/services/Publicaciones/SGIO_SUR/MapServer/3/query?where=NCOD_CENTRO%3D7&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&having=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentOnly=false&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&f=json";
 const url_avisos_agua= "https://gisprd.sedapal.com.pe/arcgis/rest/services/Publicaciones/SGIO_SUR/MapServer/2/query?where=NCOD_CENTRO%3D7&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&having=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentOnly=false&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&f=json";
@@ -234,8 +235,37 @@ const avisos_agua = (req, res) => {
                 let avisos = [];
                 avisos = response.data.features.map((datos)=>{
                     const ns = utm.convertUtmToLatLng(datos.geometry.x,datos.geometry.y,18,"");
-                    datos.attributes.gps = ns
+                    delete datos.attributes.ESRI_OID;
+                    delete datos.attributes.T_ATENCION;
+                    delete datos.attributes.F_RESOL;
+                    delete datos.attributes.T_RESOL;
+                    delete datos.attributes.F_FINALIZACION;
+                    delete datos.attributes.T_FINALIZACION;
+                    delete datos.attributes.USUARIO;
+                    delete datos.attributes.USUARIO_GESTOR;
+                    delete datos.attributes.CODIGO_ABASTEC;
+                    delete datos.attributes.NRO_LLAMADAS;
+                    delete datos.attributes.EST_SUMINISTRO;
+                    delete datos.attributes.FRECUENCIA;
+                    delete datos.attributes.TIEMPO;                    
+                    datos.attributes.gpslat = ns.lat;
+                    datos.attributes.gpslng = ns.lng;
                     //datos.geometry = ns;
+                    let valores = Object.values(datos.attributes); 
+                    let placeholders = valores.map((valores) => '?').join(',');
+                    let key = Object.keys(datos.attributes);
+                    let key_name = key.map((val) => `'${val}'`).join(',');
+
+                    let sql = `INSERT OR REPLACE INTO tbl_avisos(${key_name}) VALUES  (${placeholders})`;                   
+    
+                    db.run(sql, valores, function(err) {
+                        if (err) {
+                          return console.error(err.message);
+                        }
+                        console.log(`Fila Insertada: ${this.changes}`);
+                      });
+                    
+                    
                     return datos.attributes;//[datos.attributes, ];
                 //console.log(datos);
             });
@@ -244,7 +274,9 @@ const avisos_agua = (req, res) => {
             //response.data.features[0].geometry=ns;
             //console.log(ns);
             //res.send(response.data.features);
+
             res.send(avisos);
+            
         })
         .catch(error => {
             res.send(error);
